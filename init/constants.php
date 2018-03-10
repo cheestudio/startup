3181558f81eca909d55c3e4c1178c7f5a548a8c4
Lars Faye <lars@goodchee.com>
2018-03-10 11:16:34 -0800

    adding register sidebar function

--- init/constants.php
+++ init/constants.php
@@ -15,3 +15,15 @@ add_theme_support('post-thumbnails');
 /* Custom Image Sizes
 ========================================================= */
 //add_image_size('name', 400, 400, true);
+
+
+/* Register Sidebars
+========================================================= */
+/*register_sidebar(array(
+  'name'          => __('Primary Sidebar'),
+  'id'            => 'sidebar-primary',
+  'before_widget' => '<div class="widget %1$s %2$s"><div class="widget-inner">',
+  'after_widget'  => '</div></div>',
+  'before_title'  => '<h3>',
+  'after_title'   => '</h3>',
+  ));*/
