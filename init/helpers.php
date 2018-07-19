5445407ad26fece2ebd3cfcf853584bc76d6fc2b
Lars Faye <lars@goodchee.com>
2018-07-19 16:24:18 -0700

    custom login screen function

--- init/helpers.php
+++ init/helpers.php
@@ -4,18 +4,58 @@
 
 /* Custom Login Screen
 ========================================================= */
+
 function custom_login_screen() { ?>
-  <style type="text/css">
 
-  #login h1 a,
-  .login h1 a {
+<style type="text/css">
+
+#login h1 a, .login h1 a {
     background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/LOGO.png); padding-bottom: 0px; background-size: 100%;  width: 100%; height: 160px;
-  }
-
-  </style>
+}
+body {
+  background: #fefefe !important;
+}
+#login a {
+  color: #000 !important;
+}
+#login a:hover {
+  color: red !important;
+}
+#wp-submit {
+    display: inline-block;
+  *display: inline;
+  zoom: 1;
+  line-height: normal;
+  white-space: nowrap;
+  vertical-align: baseline;
+  text-align: center;
+  cursor: pointer;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  border: 2px solid #000;
+  color:white;
+  padding:5px 10px;
+  -webkit-transition:400ms;
+  -o-transition:400ms;
+  transition:400ms;
+  background:#000;
+  text-transform: uppercase;
+  letter-spacing: 0.03em;
+  font-size: 19px;
+  font-weight: 700;
+  border-radius: 0;
+  line-height: 0;
+  padding: 10px;
+  box-shadow: none;
+  text-shadow: none;
+}
+ </style>
 <?php }
-add_action( 'login_enqueue_scripts', 'custom_login_screen' );
 
+add_action( 'login_enqueue_scripts', 'custom_login_screen' );
 
 /* Gravity Forms anchor creation
 ========================================================= */
