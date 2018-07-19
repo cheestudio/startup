18cab14f3dd258f8afd06dcf7de6a699dc1d7e92
Lars Faye <lars@goodchee.com>
2018-07-19 11:03:30 -0700

    moving custom login screen to helpers.php

--- functions.php
+++ functions.php
@@ -7,17 +7,3 @@ require_once locate_template('/init/comments.php');    // Custom comments modifi
 require_once locate_template('/init/scripts.php');    // Theme Scripts and Stylesheets
 require_once locate_template('/init/helpers.php');   // All other custom functions
 require_once locate_template('/init/cpt.php');      // Custom Post Types
-
-/* Custom Login Screen
-========================================================= */
-function custom_login_screen() { ?>
-  <style type="text/css">
-
-  #login h1 a,
-  .login h1 a {
-    background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/LOGO.png); padding-bottom: 0px; background-size: 100%;  width: 100%; height: 160px;
-  }
-
-  </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'custom_login_screen' );
--- init/helpers.php
+++ init/helpers.php
@@ -2,6 +2,21 @@
 /* HELPER FUNCTIONS
 ========================================================= */
 
+/* Custom Login Screen
+========================================================= */
+function custom_login_screen() { ?>
+  <style type="text/css">
+
+  #login h1 a,
+  .login h1 a {
+    background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/LOGO.png); padding-bottom: 0px; background-size: 100%;  width: 100%; height: 160px;
+  }
+
+  </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'custom_login_screen' );
+
+
 /* Gravity Forms anchor creation
 ========================================================= */
 add_filter("gform_confirmation_anchor", create_function("","return false;"));
