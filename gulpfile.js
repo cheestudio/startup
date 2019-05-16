ae42868634eb87bec9e3fbe50f6877474ce95508
Lars Faye <lars@goodchee.com>
2019-05-16 10:14:57 -0700

    revising gulp CSS compiling, full conversion to cleanCSS w/minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,38 @@
 /* Variables
 ========================================================= */
 const PROJECT_URL      = 'http://localhost/NAME/',
-      ROOT             = './',
-      STYLES_MAIN      = 'assets/scss/main.scss',
-      STYLES_SOURCE    = 'assets/scss/**/*.scss',
-      JS_SOURCE        = 'assets/js/src/*.js',
-      JS_DEST          = 'assets/js/',
-      IMAGES_SOURCE    = 'assets/img/**/*',
-      IMAGES_DEST      = 'assets/img',
-      ALL_PHP          = './**/*.php',
-      BROWSER_VERSIONS = [
-        'last 2 version',
-        '> 1%',
-        'ie >= 9',
-        'ie_mob >= 10',
-        'ff >= 30',
-        'chrome >= 34',
-        'safari >= 7',
-        'opera >= 23',
-        'ios >= 7',
-        'android >= 4',
-        'bb >= 10'
-      ];
+ROOT             = './',
+STYLES_MAIN      = 'assets/scss/main.scss',
+STYLES_SOURCE    = 'assets/scss/**/*.scss',
+JS_SOURCE        = 'assets/js/src/*.js',
+JS_DEST          = 'assets/js/',
+IMAGES_SOURCE    = 'assets/img/**/*',
+IMAGES_DEST      = 'assets/img',
+ALL_PHP          = './**/*.php',
+BROWSER_VERSIONS = [
+'last 2 version',
+'> 1%',
+'ie >= 9',
+'ie_mob >= 10',
+'ff >= 30',
+'chrome >= 34',
+'safari >= 7',
+'opera >= 23',
+'ios >= 7',
+'android >= 4',
+'bb >= 10'
+];
 
 
 /* Plugins
 ========================================================= */
 let gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    mmq          = require('gulp-merge-media-queries'),
-    cleanCSS = require('gulp-clean-css'),
-    filter       = require('gulp-filter'),
+sass         = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+mmq          = require('gulp-merge-media-queries'),
+cleanCSS = require('gulp-clean-css'),
+filter       = require('gulp-filter'),
+minifyCSS    = require('gulp-uglifycss'), // Minify CSS
     uglifyJS     = require('gulp-uglify'), // Minify JS
     plumber      = require('gulp-plumber'), // Error catcher
     newer        = require('gulp-newer'),
@@ -57,25 +58,34 @@ let onError = function (err) {
 gulp.task('styles', function() {
 
   return gulp.src( STYLES_MAIN )
-    .pipe(plumber( { errorHandler: onError } ))
-    .pipe(sass( {
-      errLogToConsole: true,
-      outputStyle: 'expanded',
-      precision: 10
+  .pipe(plumber( { errorHandler: onError } ))
+  .pipe(sass( {
+    errLogToConsole: true,
+    outputStyle: 'expanded',
+    precision: 10
     } ))
     .pipe(mmq( { log: true } )) // Combines Media Queries
-   .pipe(cleanCSS({ level: { 2: {all: true}} }))
+    .pipe(cleanCSS({
+      format: 'beautify',
+      level: {
+        2: {
+          all: true,
+          mergeNonAdjacentRules: false,
+          mergeMedia: false
+        }
+      }
+      }))
     .pipe(autoprefixer( BROWSER_VERSIONS ))
     .pipe(lineec() ) // Line Endings for non-UNIX systems
     .pipe(rename('style.css'))
     .pipe(gulp.dest( ROOT ))
     .pipe(filter( 'style.css' )) // Filtering stream to only style.css
     .pipe(rename( { suffix: '.min' } ))
-   // .pipe(minifyCSS( { maxLineLen: 0 } ))
+    .pipe(minifyCSS( { maxLineLen: 0 } ))
     .pipe(plumber.stop() )
     .pipe(gulp.dest( ROOT ))
     .pipe(reload( { stream: true } )) // Inject Styles when min style file is created
-});
+    });
 
 
 /* JS Files - Task
@@ -83,13 +93,13 @@ gulp.task('styles', function() {
 gulp.task('scriptsJS', function() {
 
   return gulp.src( JS_SOURCE )
-    .pipe(plumber( { errorHandler: onError } ))
-    .pipe(concat('all.min.js'))
-    .pipe(uglifyJS() )
-    .pipe(plumber.stop() )
-    .pipe(gulp.dest( JS_DEST ))
-    .pipe(reload( { stream: true } ))
-});
+  .pipe(plumber( { errorHandler: onError } ))
+  .pipe(concat('all.min.js'))
+  .pipe(uglifyJS() )
+  .pipe(plumber.stop() )
+  .pipe(gulp.dest( JS_DEST ))
+  .pipe(reload( { stream: true } ))
+  });
 
 
 
@@ -98,7 +108,7 @@ gulp.task('scriptsJS', function() {
 gulp.task('images', function() {
 
   return gulp.src( IMAGES_SOURCE )
-    .pipe(plumber( { errorHandler: onError } ))
+  .pipe(plumber( { errorHandler: onError } ))
     .pipe(newer( IMAGES_DEST )) // Pass through newer images only
     .pipe(imagemin([
       imagemin.gifsicle( {interlaced: true} ),
@@ -106,17 +116,17 @@ gulp.task('images', function() {
       imagemin.optipng( {optimizationLevel: 7} ),
       imagemin.svgo({
         plugins: [ // https://github.com/svg/svgo#what-it-can-do
-          {removeViewBox: true},
-          {removeComments: true},
-          {removeTitle: true},
-          {removeXMLProcInst: true},
-          {cleanupIDs: false}
+        {removeViewBox: true},
+        {removeComments: true},
+        {removeTitle: true},
+        {removeXMLProcInst: true},
+        {cleanupIDs: false}
         ]
-      })
-    ]))
+        })
+      ]))
     .pipe(plumber.stop() )
     .pipe(gulp.dest( IMAGES_DEST ))
-});
+    });
 
 
 /* Broswer Sync - Task
@@ -143,12 +153,12 @@ gulp.task( 'browser-sync', function() {
         textAlign: 'center',
         zIndex: '9999'
       }
-    },
+      },
     // Use a specific port
     //port: 8080,
     injectChanges: true
+    });
   });
-});
 
 
 /* Watch Tasks
@@ -158,10 +168,10 @@ gulp.task('default', ['styles', 'scriptsJS', 'images', 'browser-sync'], function
   gulp.watch( IMAGES_SOURCE, [ 'images' ] );
   gulp.watch( JS_SOURCE, [ 'scriptsJS' ] );
   gulp.watch( ALL_PHP, reload );
-});
+  });
 
 /* Clear Gulp Cache with [ gulp clear ]
 ========================================================= */
 gulp.task('clear', function () {
   cache.clearAll();
-});
+  });
