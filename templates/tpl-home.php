f79388d5b63fc8a46ad1ace85c7a2c36a732a07c
Matt <matt@goodchee.com>
2019-05-29 15:49:20 -0700

    add default form styling

--- templates/tpl-home.php
+++ templates/tpl-home.php
@@ -59,9 +59,35 @@
     </blockquote>
     <hr>
 
-    <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada.</p>
-
+    <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.</p>
+    <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada.</p>
     <p>Donec rutrum congue leo eget malesuada. Nulla porttitor accumsan tincidunt. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem ut libero malesuada feugiat. Nulla quis lorem ut libero malesuada feugiat. Pellentesque in ipsum id orci porta dapibus.</p>
+    <hr>
+
+    <div class="gf_browser_chrome gform_wrapper" id="gform_wrapper">
+      <form>
+        <div class="gform_body">
+          <ul id="gform_fields_1" class="gform_fields top_label form_sublabel_below description_below">
+            <li id="field_1_1" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_1">Name<span class="gfield_required">*</span></label>
+              <div class="ginput_container ginput_container_text"><input name="input_1" id="input_1_1" type="text" value="" class="medium" tabindex="1000" placeholder="Name" aria-required="true" aria-invalid="false"></div>
+            </li>
+            <li id="field_1_2" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_2">Phone<span class="gfield_required">*</span></label>
+              <div class="ginput_container ginput_container_phone"><input name="input_2" id="input_1_2" type="tel" value="" class="medium" tabindex="1001" placeholder="Phone" aria-required="true" aria-invalid="false"></div>
+            </li>
+            <li id="field_1_3" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_3">Email<span class="gfield_required">*</span></label>
+              <div class="ginput_container ginput_container_email">
+                <input name="input_3" id="input_1_3" type="email" value="" class="medium" tabindex="1002" placeholder="Email" aria-required="true" aria-invalid="false">
+              </div>
+            </li>
+            <li id="field_1_4" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_4">Message<span class="gfield_required">*</span></label>
+              <div class="ginput_container ginput_container_textarea"><textarea name="input_4" id="input_1_4" class="textarea medium" tabindex="1003" placeholder="Message" aria-required="true" aria-invalid="false" rows="10" cols="50"></textarea></div>
+            </li>
+          </ul>
+        </div>
+        <div class="gform_footer top_label"> <button class="button gform_button" id="gform_submit_button_1"><span>Submit</span></button>
+        </div>
+      </form>
+    </div>
 
   </div>
 </div>
