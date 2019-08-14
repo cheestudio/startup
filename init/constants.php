84fcf3924da48f2ff572206033efc435c3543dcb
Lars Faye <lars@goodchee.com>
2019-08-14 14:59:53 -0700

    adding custom image size dropdown

--- init/constants.php
+++ init/constants.php
@@ -14,8 +14,22 @@ add_theme_support('post-thumbnails');
 
 /* Custom Image Sizes
 ========================================================= */
+if (function_exists('add_image_size')) {
 //add_image_size('NAME', X, Y, $crop);
-
+}
+add_filter('image_size_names_choose', 'insert_custom_image_sizes');
+function insert_custom_image_sizes($sizes) {
+  global $_wp_additional_image_sizes;
+  if (empty($_wp_additional_image_sizes)) {
+    return $sizes;
+  }
+  foreach ($_wp_additional_image_sizes as $id => $data) {
+    if (!isset($sizes[$id])) {
+      $sizes[$id] = ucfirst(str_replace('-', ' ', $id));
+    }
+  }
+  return $sizes;
+}
 
 /* Register Sidebars
 ========================================================= */
