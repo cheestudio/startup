2c4229f502ce84f10865ce728b44c44b0d718b04
Matt <matt@goodchee.com>
2019-09-27 17:10:44 -0700

    modify default styles

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <title><?php wp_title(''); ?></title>
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="theme-color" content="#fff">
+  <meta name="theme-color" content="#ffffff">
   <?php wp_head(); ?>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
 
