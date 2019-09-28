5689380ba55cd987f248b9290e52ee1b0dc14047
Matt <matt@goodchee.com>
2019-09-27 19:25:35 -0700

    re-factor 404 page/styles

--- 404.php
+++ 404.php
@@ -1,49 +1,30 @@
 <?php get_header(); ?>
 
 
-<?php
-
-// Look for a page slugged "404"
-
+<?php // Look for a page slug "404"
 query_posts(array(
-            'post_type' => 'page',
-            'name' => '404'
-));
+  'post_type' => 'page',
+  'name'      => '404'
+)); ?>
 
-if (have_posts()):
-     while (have_posts()) : the_post(); ?>
-
-<div class="page-404">
+<section class="page-404">
   <div class="container">
+    <div class="page-404--content">
 
-    <section class="page-404-content">
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-    </section>
+      <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+          <h1><?php the_title(); ?></h1>
+          <?php the_content(); ?>
+        <?php endwhile; ?>
 
+      <?php else : ?>
+        <h1>404 - Page Not Found</h1>
+        <h2>Do you think this is a site error?</h2>
+        <h3><strong><a href="/contact/" title="Contact us">Contact us</a></strong> and let us know so we can look into it!</h3>
+      <?php endif; ?>
+    </div>
   </div>
-</div>
-
-
-    <?php endwhile; ?>
-
-<?php else: ?>
-
-<div class="page-404">
-  <div class="container">
-
-    <section class="page-404-content">
-      <h1>404 - Page Not Found</h1>
-      <br><br>
-
-      <h3>Do you think this is a site error?</h3>
-      <h3><strong><a href="/contact/">Contact us</a></strong> and let us know so we can look into it!</h3>
-    </section>
-
-  </div>
-</div>
-
-<?php endif; ?>
+</section>
 
 
 <?php get_footer(); ?>
