35418b6d57ee72f0d85d3700c70d36d48f1a36be
Matt <matt@goodchee.com>
2019-11-24 23:37:14 -0800

    remove inserting of span tags from GF submit button

--- init/helpers.php
+++ init/helpers.php
@@ -118,7 +118,7 @@ function svg( $path, $alt='' ) {
 ========================================================= */
 add_filter( 'gform_submit_button', 'form_submit_button', 10, 2 );
 function form_submit_button( $button, $form ) {
-  return "<button class='button gform_button' id='gform_submit_button_{$form['id']}'><span>{$form['button']['text']}</span></button>";
+  return "<button class='button gform_button' id='gform_submit_button_{$form['id']}'>{$form['button']['text']}</button>";
 }
 
 
