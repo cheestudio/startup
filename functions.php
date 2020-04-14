81e9b903bcb52d82e119ebfdb78ae3fa266bc62e
Matt <matt@goodchee.com>
2020-04-14 10:33:35 -0700

    add include for custom functions

--- functions.php
+++ functions.php
@@ -4,5 +4,6 @@
 require_once locate_template('/init/shortcodes.php');    // Shortcodes
 require_once locate_template('/init/constants.php');    // Initial theme setup and constants
 require_once locate_template('/init/scripts.php');    // Theme Scripts and Stylesheets
-require_once locate_template('/init/helpers.php');   // All other custom functions
-require_once locate_template('/init/cpt.php');      // Custom Post Types
+require_once locate_template('/init/helpers.php');   // Other default theme functions
+require_once locate_template('/init/custom.php');   // Custom function additions
+require_once locate_template('/init/cpt.php');     // Custom Post Types
