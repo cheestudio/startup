e71fbeaa1266ceedb76195623a6d718d33d03533
Matt <matt@goodchee.com>
2020-05-21 18:01:06 -0700

    remove <title>

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html lang="en-US">
 <head>
   <meta charset="utf-8">
-  <title><?php wp_title(''); ?></title>
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="theme-color" content="#00704a">
