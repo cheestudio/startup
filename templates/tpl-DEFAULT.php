b556840d424c8ae9819b074784bdb73ee9733bef
Matt <matt@goodchee.com>
2020-05-21 17:58:06 -0700

    update w more grav form detault options

--- templates/tpl-DEFAULT.php
+++ templates/tpl-DEFAULT.php
@@ -5,7 +5,7 @@
 <?php get_header(); ?>
 
 
-<!-- PLACEHOLDER -->
+<!-- PLACEHOLDER BEGIN-->
 <div class="DELETE-ME">
   <div class="container">
 
@@ -15,6 +15,7 @@
     <h4>Heading #4</h4>
     <h5>Heading #5</h5>
     <h6>Heading #6</h6>
+
     <hr>
     
     <p style="font-weight: 300">Font Weight: 300</p>
@@ -23,6 +24,7 @@
     <p style="font-weight: 600">Font Weight: 600</p>
     <p style="font-weight: 700">Font Weight: 700</p>
     <p style="font-weight: 800">Font Weight: 800</p>
+
     <hr>
     
     <ul>
@@ -37,6 +39,7 @@
       <li>Ordered list item 2</li>
       <li>Ordered list item 3</li>
     </ol>
+
     <hr>
 
     <p><strong>Bold Bold Bold</strong></p>
@@ -51,40 +54,59 @@
       <a href="#" class="button">I'm a Button</a>
       <a href="#" class="button">I'm a Button</a>
     </div>
+
     <hr>
 
     <blockquote>
       <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
       <p>- Author Name</p>
     </blockquote>
+
     <hr>
 
     <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.</p>
     <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada.</p>
     <p>Donec rutrum congue leo eget malesuada. Nulla porttitor accumsan tincidunt. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem ut libero malesuada feugiat. Nulla quis lorem ut libero malesuada feugiat. Pellentesque in ipsum id orci porta dapibus.</p>
+
     <hr>
 
-    <div class="gf_browser_chrome gform_wrapper" id="gform_wrapper">
-      <form>
+    <div class="gf_browser_chrome gform_wrapper" id="gform_wrapper_1">
+      <form method="post" enctype="multipart/form-data" target="gform_ajax_frame_1" id="gform_1" action="/">
         <div class="gform_body">
           <ul id="gform_fields_1" class="gform_fields top_label form_sublabel_below description_below">
-            <li id="field_1_1" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_1">Name<span class="gfield_required">*</span></label>
-              <div class="ginput_container ginput_container_text"><input name="input_1" id="input_1_1" type="text" value="" class="medium" tabindex="1000" placeholder="Name" aria-required="true" aria-invalid="false"></div>
+            <li id="field_1_1" class="gfield gf_left_half gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_1">First Name<span class="gfield_required">*</span></label>
+              <div class="ginput_container ginput_container_text"><input name="input_1" id="input_1_1" type="text" value="" class="medium" tabindex="1" aria-required="true" aria-invalid="false"></div>
             </li>
-            <li id="field_1_2" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_2">Phone<span class="gfield_required">*</span></label>
-              <div class="ginput_container ginput_container_phone"><input name="input_2" id="input_1_2" type="tel" value="" class="medium" tabindex="1001" placeholder="Phone" aria-required="true" aria-invalid="false"></div>
+            <li id="field_1_2" class="gfield gf_right_half gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_2">Last Name<span class="gfield_required">*</span></label>
+              <div class="ginput_container ginput_container_text"><input name="input_2" id="input_1_2" type="text" value="" class="medium" tabindex="2" aria-required="true" aria-invalid="false"></div>
             </li>
-            <li id="field_1_3" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_3">Email<span class="gfield_required">*</span></label>
+            <li id="field_1_3" class="gfield field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_3">Large Size</label>
+              <div class="ginput_container ginput_container_text"><input name="input_3" id="input_1_3" type="text" value="" class="large" tabindex="3" aria-invalid="false"></div>
+            </li>
+            <li id="field_1_4" class="gfield field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_4">Message</label>
+              <div class="ginput_container ginput_container_textarea"><textarea name="input_4" id="input_1_4" class="textarea medium" tabindex="4" aria-invalid="false" rows="10" cols="50"></textarea></div>
+            </li>
+            <li id="field_1_5" class="gfield gf_left_third field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_5">Phone</label>
+              <div class="ginput_container ginput_container_phone"><input name="input_5" id="input_1_5" type="text" value="" class="medium" tabindex="5" aria-invalid="false"></div>
+            </li>
+            <li id="field_1_6" class="gfield gf_middle_third field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_6">Email</label>
               <div class="ginput_container ginput_container_email">
-                <input name="input_3" id="input_1_3" type="email" value="" class="medium" tabindex="1002" placeholder="Email" aria-required="true" aria-invalid="false">
+                <input name="input_6" id="input_1_6" type="text" value="" class="medium" tabindex="6" aria-invalid="false">
               </div>
             </li>
-            <li id="field_1_4" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_4">Message<span class="gfield_required">*</span></label>
-              <div class="ginput_container ginput_container_textarea"><textarea name="input_4" id="input_1_4" class="textarea medium" tabindex="1003" placeholder="Message" aria-required="true" aria-invalid="false" rows="10" cols="50"></textarea></div>
+            <li id="field_1_7" class="gfield gf_right_third field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_1_7">Dropdown</label>
+              <div class="ginput_container ginput_container_select"><select name="input_7" id="input_1_7" class="medium gfield_select" tabindex="7" aria-invalid="false">
+                <option value="First Choice">First Choice</option>
+                <option value="Second Choice">Second Choice</option>
+                <option value="Third Choice">Third Choice</option>
+              </select></div>
+            </li>
+            <li id="field_1_8" class="gfield field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label">Consent</label>
+              <div class="ginput_container ginput_container_consent"><input name="input_8.1" id="input_1_8_1" type="checkbox" value="1" tabindex="8" aria-invalid="false"> <label class="gfield_consent_label" for="input_1_8_1">I agree to the privacy policy.</label> <input type="hidden" name="input_8.2" value="I agree to the privacy policy." class="gform_hidden"><input type="hidden" name="input_8.3" value="1" class="gform_hidden"></div>
             </li>
           </ul>
         </div>
-        <div class="gform_footer top_label"> <button class="button gform_button" id="gform_submit_button_1"><span>Submit</span></button>
+        <div class="gform_footer top_label"> <button class="button gform_button" id="gform_submit_button_1">Submit</button> <input type="hidden" name="gform_ajax" value="form_id=1&amp;title=&amp;description=&amp;tabindex=1">
         </div>
       </form>
     </div>
