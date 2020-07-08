c0ad0c18c4e9ad4f12bec5fdf9969038c5907232
Matt <matt@goodchee.com>
2020-07-08 10:41:50 -0700

    refactor button shortcode

--- init/shortcodes.php
+++ init/shortcodes.php
@@ -1,28 +1,31 @@
 <?php
-/* Shortcodes
-========================================================= */
 
-/* Button (with on-page anchor class when arugment is used)
+/* Button
 ========================================================= */
+add_shortcode('button', 'ebutton');
 function ebutton($atts, $content = null) {
  extract(shortcode_atts(array(
   'align'  => '',
-  'url'   => '',
-  'target' => '',
-  'anchor' => ''
-  ),
- $atts));
- $output  = '<p';
- if($align){$output.=' style="text-align:'.$align.'"';}$output .= '><a';if($target){$output.=' target="'.$target.'"';}$output .= ' href="'.$url.'" class="button';if($anchor){$output.= ' same-page';}$output.='">' . do_shortcode($content) . '</a></p>';
- return $output;
+  'url'    => '',
+  'target' => ''
+), $atts));
+
+  $output = '<div class="button-wrap"';
+  if ( $align ) {
+    $output .=' style="text-align: '.$align.'"';
+  }
+  $output .= '><a';
+  if ( $target ) {
+    $output .=' target="'.$target.'"';
+  }
+  $output .= ' href="'.$url.'" class="button">'.do_shortcode( $content ).'</a></div>';
+  return $output;
 }
-add_shortcode('button', 'ebutton');
 
 /* Current Year (for use within WYSIWYG editor, such as a Copyright Date)
 ========================================================= */
+add_shortcode( 'year', 'jr_cy_y' );
 function jr_cy_y() {
   $date = getdate();
   return $date['year'];
 }
-
-add_shortcode( 'year', 'jr_cy_y' );
