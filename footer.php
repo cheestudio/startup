a87ef0a62a88820506e9a4bed8f327dfe7ec513a
Matt <matt@goodchee.com>
2020-07-23 14:20:41 -0700

    re-factor footer, add copyright field to global company info

--- footer.php
+++ footer.php
@@ -3,24 +3,13 @@ $footer_code = get_field('footer_code', 'option');
 $footer_logo = get_field('logo_footer', 'option') ? get_field('logo_footer', 'option') : get_field('logo_desktop', 'option');
 $phone       = get_field('company_phone', 'option');
 $email       = get_field('company_email', 'option');
-$address     = get_field('company_address', 'option'); ?>
+$address     = get_field('company_address', 'option');
+$copyright   = get_field('company_copyright', 'option'); ?>
 
 </main>
 
 <footer class="footer">
   <div class="container">
-    
-    <div class="footer--menu">
-      <?php include( locate_template('partials/navs/nav-footer.php') ); ?>
-    </div>
-
-    <div class="footer--social">
-      <?php include( locate_template('partials/elements/social-icons.php') ); ?>
-    </div>
-
-    <div class="footer--copyright">
-      <p>&copy; <?= date('Y'); ?></p>
-    </div>
 
     <?php if ( $footer_logo ) : ?>
       <div class="footer--logo">
@@ -29,6 +18,34 @@ $address     = get_field('company_address', 'option'); ?>
         </a>
       </div>
     <?php endif; ?>
+
+    <div class="footer--menu">
+      <?php include( locate_template('partials/navs/nav-footer.php') ); ?>
+    </div>
+
+    <div class="footer--social">
+      <?php include( locate_template('partials/elements/social-icons.php') ); ?>
+    </div>
+
+    <div class="footer--contact">
+      <?php if ( $address ) : ?>
+        <div class="address"><?= $address; ?></div>
+      <?php endif; ?>
+      <?php if ( $phone ) : ?>
+        <p class="phone">
+          <a href="tel:+1-<?= sanitize_title_with_dashes( $phone ); ?>" title="Link to Call Us"><?= $phone; ?></a>
+        </p>
+      <?php endif; ?>
+      <?php if ( $email ) : ?>
+        <p class="email">
+          <a href="mailto:<?= $email; ?>" title="Link to Email Us"><?= $email; ?></a>
+        </p>
+      <?php endif; ?>
+    </div>
+
+    <div class="footer--copyright">
+      <?= $copyright; ?>
+    </div>
 
   </div>
 </footer>
