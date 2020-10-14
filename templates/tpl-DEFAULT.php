ec96e70b361e6b96534cdf6ea72500f07c98b1fb
Matt <matt@goodchee.com>
2020-10-14 12:44:05 -0700

    add more styling examples

--- templates/tpl-DEFAULT.php
+++ templates/tpl-DEFAULT.php
@@ -48,10 +48,17 @@
 
     <p><span style="text-decoration: underline;">Underline</span> <span style="text-decoration: underline;">Underline</span> </p>
 
-    <p><a href="">Link 1</a> <a href="">Link 2</a> <a href="">Link 3</a></p>
+    <hr>
+
+    <p><a href="tel:123-456-7890">123-456-7890</a></p>
+
+    <p><a href="mailto:email@email.com">email@example.com</a></p>
+
+    <p>Inline Links: <a href="">Link 1</a> <a href="">Link 2</a></p>
+
+    <p style="display:inline">Non-Inline Links: </p> <a href="">Link 3</a> <a href="">Link 4</a>
 
     <div class="button-wrap">
-      <a href="#" class="button">I'm a Button</a>
       <a href="#" class="button">I'm a Button</a>
     </div>
 
@@ -64,7 +71,7 @@
 
     <hr>
 
-    <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.</p>
+    <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Quisque velit nisi, pretium <a href="">ut lacinia in elementum</a> id enim. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.</p>
     <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada.</p>
     <p>Donec rutrum congue leo eget malesuada. Nulla porttitor accumsan tincidunt. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem ut libero malesuada feugiat. Nulla quis lorem ut libero malesuada feugiat. Pellentesque in ipsum id orci porta dapibus.</p>
 
